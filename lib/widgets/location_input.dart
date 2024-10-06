fe4cdf90b889b49b0366cdfe408e3dc69c825621
Aviral Srivastava <kanha23srivastava@gmail.com>
2024-10-06 15:17:13 +0530

    => Getting the actual location of the user using the location package. => Showing a loading Spinner untill the location is not fetched

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -11,8 +12,59 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LoactionInputState extends State<LocationInput> {
+  Location? _pickedLocation;
+  var _isGettingLocation = false;
+  void getCurrentLocation() async {
+
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    setState(() {
+    _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+
+    setState(() {
+    _isGettingLocation = false;
+    });
+
+    print(locationData.latitude);
+    print(locationData.longitude);
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    Widget previewContent = Text(
+            'No Location Choosen',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          );
+
+    if(_isGettingLocation){
+      previewContent = const CircularProgressIndicator();
+    }      
     return Column(
       children: [
         Container(
@@ -25,20 +77,14 @@ class _LoactionInputState extends State<LocationInput> {
               color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
             ),
           ),
-          child: Text(
-            'No Location Choosen',
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
-          ),
+          child: previewContent,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
               label: const Text('Get current Location'),
-              onPressed: () {},
+              onPressed: getCurrentLocation,
               icon: const Icon(Icons.location_on_outlined),
             ),
             TextButton.icon(
